cc4d6af8f909772aa73f0153d341800d5712ef80
Tk <tfor8084@uni.sydney.edu.au>
2023-05-27 20:27:54 +1000

    movie form is able to collect and dispay data

--- public/script.js
+++ public/script.js
@@ -1,12 +1,17 @@
 // plus button java script 
 //Code taken and adapted from https://www.w3schools.com/howto/howto_js_popup_form.asp
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+
+const openButton = document.querySelector("[popUpButton]")
+const closeButton = document.querySelector("[closeButton]")
+const modal = document.querySelector(".form-popup")
+
+openButton.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    modal.close();  
+});
 
 //tab java script
 //Code taken and adapted from https://codepen.io/wanderingwind/pen/rNNwgxe
@@ -34,3 +39,56 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+// arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+// get the data from the movie-form and display it on the page
+
+const form = document.getElementById('movieForm');
+const tasklistElem = document.querySelector('.comedy-container');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    // console.log(form.elements.movieName.value);
+    addMovie(
+        form.elements.movieName.value,
+        form.elements.movieName.value,
+        form.elements.movieDuration.value,
+        form.elements.movieGenre.value,
+        form.elements.movieRating.value,
+        form.elements.movieEmojiReview.value,
+        form.elements.movieWrittenReview.value
+    );
+
+})
+
+class Movie{
+    constructor(name, duration, genre, rating, emojiReview, writtenReview){
+        this.name = name;
+        this.duration = duration;
+        this.genre = genre;
+        this.rating = rating;
+        this.emojiReview = emojiReview;
+        this.writtenReview = writtenReview;
+        this.date = new Date().toISOString();
+        this.ID = Date.now();
+    }
+    
+  }
+
+  var media = [];
+
+  function addMovie(Name, name, duration, genre, rating, emojiReview, writtenReview){
+     Name = new Movie(name, duration, genre, rating, emojiReview, writtenReview);
+     media.push(Name);
+     displaymedia(Name);
+        console.log(Name);
+        console.log(media);
+    }
+
+    function displaymedia(media) {
+        let item = document.createElement('button');
+        item.innerHTML = `<p> ${media.name} </p><br><br> <p> ${media.duration} </p><br><br> <p> ${media.genre} </p><br><br> <p> ${media.rating} </p><br><br> <p> ${media.emojiReview} </p><br><br> <p> ${media.writtenReview} </p><br><br> <p> ${media.date} </p><br><br> <p> ${media.ID} </p>`;
+        tasklistElem.appendChild(item);
+        form.reset();
+      }
+        
