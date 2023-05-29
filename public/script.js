2c499d985c48f8cbae1592eb61948e4a96e41824
Tk <tfor8084@uni.sydney.edu.au>
2023-05-29 14:50:04 +1000

    website is now able to be clearly viewed on a mobile screen

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ openButton.addEventListener('click', () => {
 });
 
 closeButton.addEventListener('click', () => {
-    modal.close();  
+    modal.close();
 });
 
 //tab java script
@@ -22,22 +22,22 @@ const tabContents = document.querySelectorAll('[data-tab-content]')
 
 //Create a forEach loop to go through each of the tabs and check if they've been clicked
 tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
+    tab.addEventListener('click', () => {
 
-    //If the tab has been clicked we'll find what the target was first
-    const target = document.querySelector(tab.dataset.tabTarget)
-    
-    //Remove active class from all tabs and tab contents
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
+        //If the tab has been clicked we'll find what the target was first
+        const target = document.querySelector(tab.dataset.tabTarget)
+
+        //Remove active class from all tabs and tab contents
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        //Add active class to the tab which has been clicked and it's relevent page content
+        tab.classList.add('active')
+        target.classList.add('active')
     })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    //Add active class to the tab which has been clicked and it's relevent page content
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
 })
 
 // arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
@@ -47,8 +47,8 @@ const movieForm = document.getElementById('movieForm');
 const tvForm = document.getElementById('tvForm');
 const tasklistElem = document.querySelector('.comedy-container');
 
-movieForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+movieForm.addEventListener('submit', function (event) {
+    event.preventDefault();
     addMovie(
         movieForm.elements.movieName.value,
         movieForm.elements.movieName.value,
@@ -60,8 +60,8 @@ movieForm.addEventListener('submit', function(event) {
     );
 
 })
-tvForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+tvForm.addEventListener('submit', function (event) {
+    event.preventDefault();
     addTv(
         tvForm.elements.tvName.value,
         tvForm.elements.tvName.value,
@@ -76,8 +76,8 @@ tvForm.addEventListener('submit', function(event) {
 
 })
 
-class Movie{
-    constructor(name, duration, genre, rating, emojiReview, writtenReview){
+class Movie {
+    constructor(name, duration, genre, rating, emojiReview, writtenReview) {
         this.name = name;
         this.duration = duration;
         this.genre = genre;
@@ -87,46 +87,75 @@ class Movie{
         this.date = new Date().toISOString();
         this.ID = Date.now();
     }
-    
-  }
 
-  class Tv extends Movie{
-    constructor(name, episode, season, duration, genre, rating, emojiReview, writtenReview,){
+}
+
+class Tv extends Movie {
+    constructor(name, episode, season, duration, genre, rating, emojiReview, writtenReview,) {
         super(name, duration, genre, rating, emojiReview, writtenReview);
         this.episode = episode;
         this.season = season;
     }
-  }
+}
 
-  var media = [];
+var media = [];
 
-  function addMovie(Name, name, duration, genre, rating, emojiReview, writtenReview){
-     Name = new Movie(name, duration, genre, rating, emojiReview, writtenReview);
-     media.push(Name);
-     displaymedia(Name);
-        console.log(Name);
-        console.log(media);
-    }
+function addMovie(Name, name, duration, genre, rating, emojiReview, writtenReview) {
+    Name = new Movie(name, duration, genre, rating, emojiReview, writtenReview);
+    media.push(Name);
+    displaymedia(Name);
+    console.log(Name);
+    console.log(media);
+}
 
-    function addTv(Name, name, episode, season, duration, genre, rating, emojiReview, writtenReview){
-        Name = new Tv(name, episode, season, duration, genre, rating, emojiReview, writtenReview);
-        media.push(Name);
-        displaymedia(Name);
-            console.log(Name);
-            console.log(media);
-    }
+function addTv(Name, name, episode, season, duration, genre, rating, emojiReview, writtenReview) {
+    Name = new Tv(name, episode, season, duration, genre, rating, emojiReview, writtenReview);
+    media.push(Name);
+    displaymedia(Name);
+    console.log(Name);
+    console.log(media);
+}
 
-    function displaymedia(media) {
-        let item = document.createElement('button');
-        if (media instanceof Tv){
-            item.innerHTML = `<p> ${media.name} </p><p> ${media.episode}</p><p> ${media.season} </p><p> ${media.duration}</p>p> ${media.genre} </p><p> ${media.rating} </p><p> ${media.emojiReview} </p><p> ${media.writtenReview}</p>`;
-        tasklistElem.appendChild(item);        
-        tvForm.reset();
-      }
-        else if (media instanceof Movie){
-            item.innerHTML = `<p> ${media.name} </p><p> ${media.duration}</p>p> ${media.genre} </p><p> ${media.rating} </p><p> ${media.emojiReview} </p><p> ${media.writtenReview}</p>`;
+function displaymedia(media) {
+    let item = document.createElement('button');
+    item.setAttribute('id','description');
+    let me = document.createElement('modal');
+    me.setAttribute('id','modalHE');
+
+
+    if (media instanceof Tv) {
+        item.innerHTML = `<p> ${media.name} </p><p> ${media.episode}</p><p> ${media.season} </p><p> ${media.duration}</p>p> ${media.genre} </p><p> ${media.rating} </p><p> ${media.emojiReview} </p><p> ${media.writtenReview}</p>`;
         tasklistElem.appendChild(item);
-        movieForm.reset();
-        }
-        
+        modal.innerHTML = `<p>its opens</p><button id="closeMedia">close</button>`
+        // deleteItem(item);
+        tvForm.reset();
     }
+    else if (media instanceof Movie) {
+        item.innerHTML = `<p> ${media.name} </p><p> ${media.duration}</p><p> ${media.genre} </p><p> ${media.rating} </p><p> ${media.emojiReview} </p><p> ${media.writtenReview}</p>`;
+        tasklistElem.appendChild(item);
+        deleteItem(item);
+        movieForm.reset();
+    }
+}
+
+function deleteItem(item) {
+    let delButton = document.createElement('button');
+    let delButtonText = document.createTextNode('Delete');
+    delButton.appendChild(delButtonText);
+    item.appendChild(delButton);
+    delButton.addEventListener('click', function (event) {
+        item.remove();
+    })
+}
+
+const openMedia = document.getElementById("description")
+const closeMedia = document.getElementById("closeMedia")
+const modalHE = document.querySelector(".modalHE")
+
+openMedia.addEventListener('click', () => {
+    modalHE.showModal();
+});
+
+closeMedia.addEventListener('click', () => {
+    modalHE.close();
+});
